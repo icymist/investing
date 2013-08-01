9df8273a3c62e5f6b42ffc266b7c8d2d1962b0dd
Chaitanya Krishna Ande <icymist@gmail.com>
2013-08-01 02:43:18 +0200

    sip function is working now, but needs more refinement.

--- indices.py
+++ indices.py
@@ -5,6 +5,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
 
 index_file = lambda index: os.path.join(indices_dir, index + '.csv')
 
@@ -25,17 +26,30 @@ class Index:
         else:
             return self._df
 
-    def sip(self, date=None, freq='MS'):
-        return self._df[self._df.index > date].close.resample(freq)
+    def sip(self, date=None, freq='MS', value=1000):
+        print date
+        closing_prices = self._df[self._df.index > date].close.resample(freq)
+        units = value/closing_prices
+        df = pd.DataFrame({'price': closing_prices, 'units': units})
+        return df
 
 
 if __name__ == '__main__':
-    start_date = (2003, 1, 1)
+    start_date = (1999, 1, 1)
     sensex = Index('bse_sensex')
     sensex_data = sensex.start(start_date)
-    sip = sensex.sip(date=datetime(2000, 1, 1))
-    sip.plot()
-    plt.show()
+    sip = sensex.sip(date=datetime(2003, 06, 10), freq='AS', value=10000)
+    total_units = sip.units.sum()
+    print total_units
+    present_value = total_units*sensex_data.close[-1]
+    print 'present value', present_value
+    print len(sip)
+    rate_of_return = np.rate(len(sip), -10000, 0, present_value)
+    print 'return', (1+rate_of_return)
+    print 'return on index', ((sensex_data.close[-1]/sensex_data.close[0])**(1./len(sip)))
+    print sip.head()
+    #sip.plot()
+    #plt.show()
     #bse100 = Index('bse_100').start(start_date)
     #bse500 = Index('bse_500').start(start_date)
     
